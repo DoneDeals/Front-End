57b7c78de2e5f2b248a88070b920578309354ae3
Greg Chlosta <grzegorz.chlosta@gmail.com>
2020-12-12 14:33:27 -0600

    user auth, navbar, login page, register page, profile page

--- src/App.js
+++ src/App.js
@@ -3,18 +3,22 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import HomeScreen from './screens/HomeScreen'
+import LoginScreen from './screens/LoginScreen'
+import RegisterScreen from './screens/RegisterScreen'
+import ProfileScreen from './screens/ProfileScreen'
 
-function App() {
+export default function App() {
   return (
     <Router>
       <Header />
-      <main className='py-3'>
+      <main className='py-5'>
         <Container>
           <Route path='/' component={HomeScreen} exact />
+          <Route path='/login' component={LoginScreen} />
+          <Route path='/register' component={RegisterScreen} />
+          <Route path='/profile' component={ProfileScreen} />
         </Container>
       </main>
     </Router>
   )
 }
-
-export default App
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,19 @@
 import React from 'react'
-import { Navbar, Nav, Container } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
 import { LinkContainer } from 'react-router-bootstrap'
-import { FaUser } from 'react-icons/fa'
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
+import { FaSignInAlt } from 'react-icons/fa'
+import { logout } from '../actions/userActions'
 
 export default function Header() {
+  const dispatch = useDispatch()
+
+  const { userAuth } = useSelector((state) => state.userLogin)
+
+  const logoutHandler = () => {
+    dispatch(logout())
+  }
+
   return (
     <header>
       <Navbar bg='primary' variant='dark' expand='lg' collapseOnSelect>
@@ -14,11 +24,22 @@ export default function Header() {
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ml-auto'>
-              <LinkContainer to='/login'>
-                <Nav.Link>
-                  <FaUser /> Log In
-                </Nav.Link>
-              </LinkContainer>
+              {userAuth ? (
+                <NavDropdown title={userAuth.name} id='username'>
+                  <LinkContainer to='/profile'>
+                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                  </LinkContainer>
+                  <NavDropdown.Item onClick={logoutHandler}>
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                <LinkContainer to='/login'>
+                  <Nav.Link>
+                    <FaSignInAlt /> Sign In
+                  </Nav.Link>
+                </LinkContainer>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
