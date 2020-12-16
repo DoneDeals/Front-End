e3169a078db0379b822691a24ce7ccd42558cd63
SuilarsoJapit <suilarso.japit@skipthedishes.ca>
2020-12-16 15:51:07 -0600

    Initial file for items table

---
+++ src/components/ItemMgmtTable.js
@@ -0,0 +1,73 @@
+import React from 'react'
+import PopulatedTable from '../components/PopulatedTable';
+import paged from '../components/Pagination';
+
+
+const styles = {
+  category: {
+    whiteSpace: 'no-wrap',
+  },
+  description: {
+    whiteSpace: 'no-wrap',
+  },
+  uprice: {
+    textAlign: 'center',
+  },
+};
+
+const dummyData = [
+  {
+    category: 'Clothes',
+    description: 'Polo T-shirt',
+    uprice: 12.3
+  },
+  {
+    category: 'Toys',
+    description: 'Lego brick',
+    uprice: 45.6
+  },
+  {
+    category: 'Electronics',
+    description: 'Skull headphone',
+    uprice: 78.9
+  },
+];
+
+const columns = () => {
+  return [
+
+    {
+      header: 'Category',
+      cell: row => <div>{row.name}</div>,
+      style: styles.category,
+    },
+    {
+      header: 'Description',
+      cell: row => <div>{row.email}</div>,
+      style: styles.description,
+    },
+    {
+      header: 'U Price',
+      cell: row => <div>{row.type}</div>,
+      style: styles.uprice,
+    },
+
+  ]
+}
+
+const Table = paged(PopulatedTable);
+
+const ItemMgmtTable = ({
+  pageSize,
+  loading,
+}) => (
+  <div>
+    <Table
+      pageSize={pageSize}
+      columns={columns}
+      loading={loading}
+      dataSelector={dummyData}
+      items={3}
+    />
+  </div>
+);
