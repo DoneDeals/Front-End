73dc439087300fb7b83a17d63039589b9ca93dc1
SuilarsoJapit <suilarso.japit@skipthedishes.ca>
2021-01-03 17:52:40 -0600

    Remove comments

--- src/reducers/itemReducers.js
+++ src/reducers/itemReducers.js
@@ -3,6 +3,9 @@ import {
   ITEM_ADD_ITEM_SUCCESS,
   ITEM_ADD_ITEM_FAIL,
   ITEM_ADD_ITEM_RESET,
+  ITEM_GET_ITEM_DETAILS_REQUEST,
+  ITEM_GET_ITEM_DETAILS_SUCCESS,
+  ITEM_GET_ITEM_DETAILS_FAIL,
 } from '../constants/itemConstants';
 
 export function addItemReducer(state = {}, action) {
@@ -26,12 +29,6 @@ export function addItemReducer(state = {}, action) {
 
 }
 
-//----------------------
-
-// USER_DETAILS_REQUEST,   ITEM_GET_ITEM_DETAILS_REQUEST
-// USER_DETAILS_SUCCESS,  ITEM_GET_ITEM_DETAILS_SUCCESS
-// USER_DETAILS_FAIL,   ITEM_GET_ITEM_DETAILS_FAIL
-
 export function itemGetDetailsReducer(state = {}, action) {
   
   switch (action.type) {
@@ -53,5 +50,4 @@ export function itemGetDetailsReducer(state = {}, action) {
 
 }
 
-//-------------------
 
